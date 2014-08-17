5151448c3ea2b40d268353de5970c271c3157c05
Mathieu Lutfy <mathieu@bidon.ca>
2014-08-17 15:35:23 -0400

    Init

---
+++ symbiocivicrm.php
@@ -0,0 +1,112 @@
+<?php
+
+require_once 'symbiocivicrm.civix.php';
+
+/**
+ * Implementation of hook_civicrm_config
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_config
+ */
+function symbiocivicrm_civicrm_config(&$config) {
+  _symbiocivicrm_civix_civicrm_config($config);
+
+  // [ML] Il faut quand même configurer cette option pour CiviCase,
+  // car ça vérifie explicitement ce répertoire pour les fichiers xml.
+  $config->customTemplateDir = dirname(__FILE__) . DIRECTORY_SEPARATOR . 'templates';
+}
+
+/**
+ * Implementation of hook_civicrm_xmlMenu
+ *
+ * @param $files array(string)
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
+ */
+function symbiocivicrm_civicrm_xmlMenu(&$files) {
+  _symbiocivicrm_civix_civicrm_xmlMenu($files);
+}
+
+/**
+ * Implementation of hook_civicrm_install
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
+ */
+function symbiocivicrm_civicrm_install() {
+  return _symbiocivicrm_civix_civicrm_install();
+}
+
+/**
+ * Implementation of hook_civicrm_uninstall
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
+ */
+function symbiocivicrm_civicrm_uninstall() {
+  return _symbiocivicrm_civix_civicrm_uninstall();
+}
+
+/**
+ * Implementation of hook_civicrm_enable
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_enable
+ */
+function symbiocivicrm_civicrm_enable() {
+  return _symbiocivicrm_civix_civicrm_enable();
+}
+
+/**
+ * Implementation of hook_civicrm_disable
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
+ */
+function symbiocivicrm_civicrm_disable() {
+  return _symbiocivicrm_civix_civicrm_disable();
+}
+
+/**
+ * Implementation of hook_civicrm_upgrade
+ *
+ * @param $op string, the type of operation being performed; 'check' or 'enqueue'
+ * @param $queue CRM_Queue_Queue, (for 'enqueue') the modifiable list of pending up upgrade tasks
+ *
+ * @return mixed  based on op. for 'check', returns array(boolean) (TRUE if upgrades are pending)
+ *                for 'enqueue', returns void
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
+ */
+function symbiocivicrm_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
+  return _symbiocivicrm_civix_civicrm_upgrade($op, $queue);
+}
+
+/**
+ * Implementation of hook_civicrm_managed
+ *
+ * Generate a list of entities to create/deactivate/delete when this module
+ * is installed, disabled, uninstalled.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
+ */
+function symbiocivicrm_civicrm_managed(&$entities) {
+  return _symbiocivicrm_civix_civicrm_managed($entities);
+}
+
+/**
+ * Implementation of hook_civicrm_caseTypes
+ *
+ * Generate a list of case-types
+ *
+ * Note: This hook only runs in CiviCRM 4.4+.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
+ */
+function symbiocivicrm_civicrm_caseTypes(&$caseTypes) {
+  _symbiocivicrm_civix_civicrm_caseTypes($caseTypes);
+}
+
+/**
+ * Implementation of hook_civicrm_alterSettingsFolders
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
+ */
+function symbiocivicrm_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
+  _symbiocivicrm_civix_civicrm_alterSettingsFolders($metaDataFolders);
+}
