0a4825769d9d120b8e3acca258a709ce6817242b
mathieu Lu <mathieu@civicrm.org>
2025-09-29 17:04:51 +0200

    Welcome: cc the related contacts, add everyone as activity targets

--- Civi/Api4/Action/Symbiocivicrm/Welcome.php
+++ Civi/Api4/Action/Symbiocivicrm/Welcome.php
@@ -51,6 +51,25 @@ class Welcome extends \Civi\Api4\Generic\AbstractAction {
       'id' => $contact_id,
     ]);
 
+    // If the contribution is linked to an Organization, cc all employees
+    // This works for Spark because it's mostly new orgs, but we should fix to check
+    // for a soft-credit.
+    $cc = [];
+    $target_contact_ids = [];
+    $target_contact_ids[] = $contact_id;
+
+    if ($contact['contact_type'] == 'Organization') {
+      $relationships = \Civi\Api4\Relationship::get(FALSE)
+        ->addSelect('contact_id_a', 'contact_id_a.email_primary.email')
+        ->addWhere('relationship_type_id:name', '=', 'Employee of')
+        ->addWhere('contact_id_b', '=', $contact_id)
+        ->execute();
+      foreach ($relationships as $rel) {
+        $cc[] = $rel['contact_id_a.email_primary.email'];
+        $target_contact_ids[] = $rel['contact_id_a'];
+      }
+    }
+
     // @todo Use the site language and send translated template, if available
     $msg_template_id = \Civi::settings()->get('symbiocivicrm_welcome_template_id');
 
@@ -65,19 +84,22 @@ class Welcome extends \Civi\Api4\Generic\AbstractAction {
       'from' => "CiviCRM Spark <spark@civicrm.org>", // @todo setting? "$domainValues[0] <$domainValues[1]>",
       'toEmail' => $contact['email'],
       'toName' => $contact['display_name'],
-      'bcc' => 'mathieu@civicrm.org,mathieu@bidon.ca,josh@civicrm.org', // @todo setting
+      'cc' => implode(',', $cc),
+      'bcc' => 'mathieu@civicrm.org,josh@civicrm.org', // @todo setting
     ];
 
     list($sent, $subject, $text, $html) = \CRM_Core_BAO_MessageTemplate::sendTemplate($sendTemplateParams);
     \Civi::log()->info('Symbiocrm/Welcome: sending email to ' . $contact['email'] . ', link=' . $this->loginurl . ', result = ' . $sent);
 
     // Create an Email Activity
-    civicrm_api3('Activity', 'create', [
-      'activity_type_id' => 3, // Email
-      'source_contact_id' => $contact_id,
-      'subject' => $subject,
-      'details' => $html,
-    ]);
+    $source_contact_id = \CRM_Core_BAO_Domain::getDomain()->contact_id;
+    \Civi\Api4\Activity::create(FALSE)
+      ->addValue('activity_type_id', 3)
+      ->addValue('target_contact_id', $target_contact_ids)
+      ->addValue('source_contact_id', $source_contact_id)
+      ->addValue('subject', $subject ?? 'Spark Welcome')
+      ->addValue('details', $html ?? 'Spark Welcome')
+      ->execute();
 
     $result->exchangeArray(['sent' => $sent]);
   }
